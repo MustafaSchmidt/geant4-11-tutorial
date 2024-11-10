2265ed9fe55648d0a92b863cc2024a917cc435f4
Mustafa Schmidt <mustafa_schmidt@gmx.de>
2024-11-10 18:38:24 +0100

    First commit

---
+++ sim.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "G4RunManager.hh"
+#include "G4MTRunManager.hh"
+#include "G4UImanager.hh"
+#include "G4VisManager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+int main(int argc, char** argv)
+{
+    G4UIExecutive *ui = new G4UIExecutive(argc, argv);
+
+    #ifdef G4MULTITHREADED
+        G4MTRunManager *runManager = new G4MTRunManager;
+    #else
+        G4RunManager *runManger = new G4RunManager;
+    #endif
+
+    G4VisManager *visManager = new G4VisExecutive();
+    visManager->Initialize();
+
+    G4UImanager *UImanager = G4UImanager::GetUIpointer();
+
+    ui->SessionStart();
+
+    return 0;
+}
